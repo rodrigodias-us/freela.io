c5924a66219f3161c221b372a00b865471de58be
Rodrigo Gomes Dias <rodrigogmdias@gmail.com>
2018-01-03 17:03:09 -0200

    FavIcon

--- internals/scripts/publish.js
+++ internals/scripts/publish.js
@@ -1,7 +1,7 @@
 const shell = require('shelljs');
 const ghpages = require('gh-pages');
 
-ghpages.publish('build', {dotfiles: true}, function(err) {
+ghpages.publish('build', {dotfiles: true, add: true}, function(err) {
   if (err) {
     shell.echo(err.message);
   } else {
